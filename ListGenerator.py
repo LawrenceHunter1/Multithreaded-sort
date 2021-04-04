837bd7637de8b6b367b89afee84fa11d69fa16d4
Lawrence Hunter <lawrencehunter@Lawrences-MacBook-Pro.local>
2021-04-05 00:06:33 +0100

    Implemented better command line tool

--- ListGenerator.py
+++ ListGenerator.py
@@ -1,7 +1,16 @@
-import random
+import random, sys, argparse
 
-f = open("list_in.txt", "w+")
+if __name__ == '__main__':
 
-for i in range(1000):
-    f.write(str(random.randint(0, 100)))
-    f.write("\n")
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-n', '--items', default=1000, help="Number of items in generated list")
+    parser.add_argument('-r', '--range', default=100, help="Upper bound of number generator")
+    args = parser.parse_args()
+    n = int(args.items)
+    r = int(args.range)
+
+    f = open("list_in.txt", "w+")
+
+    for i in range(n):
+        f.write(str(random.randint(0, r)))
+        f.write("\n")
