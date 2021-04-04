88bd615c6f01e29d3ad1e69517300072f7389325
Lawrence Hunter <lawrencehunter@Lawrences-MacBook-Pro.local>
2021-04-04 21:38:26 +0100

    Splits list and incorporates status checking

--- Controller.java
+++ Controller.java
@@ -1,12 +1,30 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
 public class Controller {
     public static void main(String[] args) {
-        Node N1 = new Node(new int[] { 0, 1 });
-        Node N2 = new Node(new int[] { 2, 3 });
-        N1.start();
-        N2.start();
-        System.out.println(N1.get_result());
-        System.out.println(N2.get_result());
-        N1.interrupt();
-        N2.interrupt();
+        int[] data = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        List<Node> nodes = new ArrayList<Node>();
+        List<Integer> results = new ArrayList<Integer>();
+        for (int i = 0; i < data.length; i += 2) {
+            nodes.add(new Node(Arrays.copyOfRange(data, i, i + 2)));
+        }
+        for (Node node : nodes) {
+            node.start();
+        }
+        for (Node node : nodes) {
+            while (!node.check_status()) {
+                try {
+                    TimeUnit.MILLISECONDS.sleep(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            results.add(node.get_result());
+            node.interrupt();
+        }
+        System.out.println(results);
     }
 }
--- Node.java
+++ Node.java
@@ -1,8 +1,11 @@
+import java.util.Arrays;
+
 class Node extends Thread {
 
     private Thread t;
     private int[] data;
     private int result;
+    private boolean finished = false;
 
     Node(int[] data_in) {
         this.data = data_in;
@@ -10,11 +13,17 @@ class Node extends Thread {
 
     @Override
     public void run() {
+        System.err.println("Node running with data: " + Arrays.toString(data));
         result = data[0] + data[1];
+        finished = true;
     }
 
     public int get_result() {
         return result;
+    }
+
+    public boolean check_status() {
+        return finished;
     }
 
     public void start() {
